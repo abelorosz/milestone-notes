cdfeee0486f72d16f88bd9ef44161859b9d13546
Abel Orosz <abel@abelorosz.com>
2023-06-19 08:46:23 +0000

    feat: finish release notes generation

--- index.js
+++ index.js
@@ -1,21 +1,112 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+const fs = require('fs');
 
+const token = core.getInput('token', { required: true });
+const octokit = github.getOctokit(token);
+const context = github.context;
 
-// most @actions toolkit packages have async methods
-async function run() {
+const owner = core.getInput('owner') || process.env.GITHUB_REPOSITORY_OWNER;
+const repo = core.getInput('repo') || process.env.GITHUB_REPOSITORY_NAME;
+const milestoneNumber = context.payload.milestone.number;
+
+const labelMapping = {
+  bug: "Bug Fixes",
+  enhancement: "New Features",
+  feature: "New Features",
+};
+
+const noLabelGroup = 'Closed Issues';
+
+const query = `
+  query($owner: String!, $repo: String!, $milestoneNumber: Int!) {
+    repository(owner: $owner, name: $repo) {
+      milestone(number: $milestoneNumber) {
+        issues(first: 100) {
+          nodes {
+            number
+            title
+            url
+            author {
+              login
+            }
+            labels(first: 5) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const generateReleaseNotes = async () => {
+  if (!owner || !repo || !milestoneNumber || !token) {
+    core.error('Missing required context variables: repo owner, repo name, milestone number or repo token.');
+    core.setFailed('Action failed due to missing input/context variables.');
+    return;
+  }
+
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const data = await octokit.graphql(query, { owner, repo, milestoneNumber });
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    if (!data || !data.repository || !data.repository.milestone) {
+      core.error('Invalid response from GitHub API');
+      core.setFailed('Action failed due to invalid API response.');
+      return;
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const issues = data.repository.milestone.issues.nodes;
+    let issueGroups = new Map();
+
+    // Organize issues by label
+    issues.forEach((issue) => {
+      if (issue.labels.nodes.length === 0) {
+        // Handle issues with no labels
+        if (!issueGroups.has(noLabelGroup)) {
+          issueGroups.set(noLabelGroup, []);
+        }
+        issueGroups.get(noLabelGroup).push(issue);
+      } else {
+        // Handle issues with labels
+        issue.labels.nodes.forEach((label) => {
+          const mappedLabel = labelMapping[label.name] || label.name;
+
+          if (!issueGroups.has(mappedLabel)) {
+            issueGroups.set(mappedLabel, []);
+          }
+
+          const alreadyAdded = issueGroups.get(mappedLabel).some(groupedIssue => groupedIssue.number === issue.number);
+          if (!alreadyAdded) {
+            issueGroups.get(mappedLabel).push(issue);
+          }
+        });
+      }
+    });
+
+    let releaseNotes = '';
+
+    // Generate markdown output
+    for (let [label, issues] of issueGroups) {
+      releaseNotes += `### ${label}\n\n`;
+
+      issues.forEach((issue) => {
+        releaseNotes += `- [#${issue.number}](${issue.url}) ${issue.title} (reported by @${issue.author.login})\n`;
+      });
+
+      releaseNotes += '\n';
+    }
+
+    fs.writeFileSync('RELEASE_NOTES.md', releaseNotes);
+    core.info('Release notes have been successfully generated!');
+
   } catch (error) {
+    core.error(`Error generating release notes: ${error.message}`);
     core.setFailed(error.message);
   }
-}
+};
 
-run();
+generateReleaseNotes();
+module.exports = generateReleaseNotes;
